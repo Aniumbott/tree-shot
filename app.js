b9abf22e49e0e0b697f8e668824e645f353cee2c
Aniumbott <aniketnareshkumarrana@gmail.com>
2022-01-23 22:06:35 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector(".overlay-texts");
+const links = intro.querySelector(".look-up");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 3300,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+// Link Animation
+const linkAnim = TweenMax.fromTo(links, 3, { opacity: 0 }, { opacity: 1 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 200,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+let scene3 = new ScrollMagic.Scene({
+  duration: 200,
+  offset: 3100,
+  triggerHook: 0,
+})
+  .setTween(linkAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  video.currentTime = delay;
+}, 33.3);
